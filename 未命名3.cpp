88e1f50ddab0ea89b95c1f6c433c1217bd789b2d
20191864209 <56298977+20191864209@users.noreply.github.com>
2019-10-10 20:45:59 +0800

    Add files via upload

---
+++ 未命名3.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{int star,n;
+printf("ÇëÊäÈë±ß³¤\n");
+scanf("%d",&star);
+int i=star;
+int j; 
+while(i>0)
+{j=star;
+while(j>0)
+{
+   printf("*");
+   printf(" ");
+j--;
+}
+printf("\n");
+ i--; } 
+  return 0; 
+  }
+
